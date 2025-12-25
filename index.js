a6df655c110dad2372d702ba248d550ae9b8f5fe
Hrishikesh <hrishigaonkar2@gmail.com>
2025-12-25 16:07:47 +0530

    no twilio

--- index.js
+++ index.js
@@ -17,11 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-//twillio
-const twilioClient = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
+
 
 // ------------------------------------
 // Cloudinary Configuration
